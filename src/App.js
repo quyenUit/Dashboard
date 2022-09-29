3080112b6c7953518812aba09e3d16de79e1473c
quyenUit <nguyenquyenboss@gmail.com>
2022-09-29 23:15:43 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import "./App.css";
 import MainDash from "./components/MainDash/MainDash";
 import Sidebar from "./components/Sidebar/Sidebar";
 import RightSide from "./components/RightSide/RightSide";
+//import SignIn from "./components/SignIn/SignIn";
+import "./components/SignIn/SignIn.css";
+// import SignUp from "./components/SignUp/SignUp";
+// import "./components/SignUp/SignUp.css";
+
 function App() {
   return (
+    // <div className="">
+    //   <SignUp />
+    // </div>
     <div className="App">
       <div className="AppGlass">
         <Sidebar />
